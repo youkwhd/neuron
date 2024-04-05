31dfd8ab5a5f61dd1fe1a18a1dd45867150564c3
youkwhd <lolywk@tutanota.com>
2024-04-06 04:48:11 +0700

    chore: we have support for this, why not

--- lib/Math.jl
+++ lib/Math.jl
@@ -1,7 +1,11 @@
 module Math
 
 sigmoid(x) = 1 / (1 + exp(-x))
+σ = sigmoid
+
 sigmoid_derivative(x) = sigmoid_derivativef(sigmoid(x))
 sigmoid_derivativef(x) = x * (1 - x)
+σd = sigmoid_derivative
+σdf = sigmoid_derivativef
 
 end
