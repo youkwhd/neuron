3303d9299d54fbda7ee44aed960412959a38a425
youkwhd <lolywk@tutanota.com>
2024-04-06 21:54:13 +0700

    feat: add forward(), type should be Float

--- lib/Neuron.jl
+++ lib/Neuron.jl
@@ -1,19 +1,21 @@
 module Neuron
+    include("Math.jl")
+
     using Test
 
     mutable struct Layer
-        neurons :: Vector{Int8}
-        weights :: Matrix{Int8}
+        neurons :: Vector{Float64}
+        weights :: Matrix{Float64}
 
-        Layer(len :: Int) = new(Vector{Int8}(undef, len), Matrix{Int8}(undef, len, 1))
+        Layer(len :: Int) = new(Vector{Float64}(undef, len), Matrix{Float64}(undef, len, 1))
         Layer(len :: Int, weights) = begin
             nrows, _ = size(weights)
             @test nrows == len
 
-            new(Vector{Int8}(undef, len), weights)
+            new(Vector{Float64}(undef, len), weights)
         end
 
-        Layer(neurons) = new(neurons, Matrix{Int8}(undef, length(neurons), 1))
+        Layer(neurons) = new(neurons, Matrix{Float64}(undef, length(neurons), 1))
         Layer(neurons, weights) = begin 
             nrows, _ = size(weights)
             @test nrows == length(neurons)
@@ -26,5 +28,12 @@ module Neuron
         layers :: Array{Layer}
 
         Network(layers) = new(layers)
+    end
+
+    function forward(nn :: Network)
+        for i in 2:length(nn.layers)
+            A = reshape(nn.layers[i - 1].neurons, (1, :)) * nn.layers[i - 1].weights
+            nn.layers[i].neurons = map(Math.σ, vec(A))
+        end
     end
 end
