f7dd2a5c7ea78e21e8186fed38100805d3131178
youkwhd <lolywk@tutanota.com>
2024-04-10 01:06:08 +0700

    chore: add ReLU function

--- lib/Math.jl
+++ lib/Math.jl
@@ -1,5 +1,13 @@
 module Math
 
+ReLU(x) = max(0, x)
+
+# x == 0 should be an anomaly,
+# but for the sake of simplicty just interpret 0 to be the result
+# 
+# see: https://stats.stackexchange.com/a/333400
+ReLU_derivative(x) = Int(x > 0)
+
 sigmoid(x) = 1 / (1 + exp(-x))
 σ = sigmoid
 
