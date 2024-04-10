ea4fd23cc62e8d20253fb5eba5824baf1b2ae26d
youkwhd <lolywk@tutanota.com>
2024-04-10 21:14:17 +0700

    feat(wip): broken ass backprop learning

--- lib/Neuron.jl
+++ lib/Neuron.jl
@@ -30,24 +30,34 @@ module Neuron
     end
 
     # calculate loss using the Mean Squared Error (MSE) method
-    function loss(nn :: Network, expected_output :: Vector{Int})
-        output_neurons_len = length(last(nn.layers).neurons)
-        @test length(expected_output) == output_neurons_len
+    function loss(nn :: Network, expected_output :: Vector{T}) where T<:Number
+        expected_output_length = length(expected_output)
+        @test expected_output_length == length(last(nn.layers).neurons)
 
-        losses = map((neuron, expected) -> (expected - neuron) ^ 2,
-                    last(nn.layers).neurons, expected_output)
-
-        return (1 / output_neurons_len) * sum(losses)
+        losses = map((neuron, expected) -> (neuron - expected) ^ 2, last(nn.layers).neurons, expected_output)
+        return sum(losses)
     end
 
-    function forward(nn :: Network, activation_fn :: Function = x -> x)
+    function forward(nn :: Network, activation_fn :: Function)
         for i in 2:length(nn.layers)
             product = reshape(nn.layers[i - 1].neurons, (1, :)) * nn.layers[i - 1].weights
             nn.layers[i].neurons = map(activation_fn, vec(product))
         end
     end
 
-    function adjust(nn :: Network, activation_derivative_fn :: Function = x -> x)
+    function adjust(nn :: Network, activation_derivative_fn :: Function, expected_output :: Vector{T}) where T<:Number
+        # TODO: refactor learning_rate
+        __LEARNING_RATE = 0.1
+
+        der_losses = map((neuron, expected) -> 2 * (neuron - expected), last(nn.layers).neurons, expected_output)
+        der_loss = sum(der_losses)
+
+        for i in range(length(nn.layers), 2, step=-1)
+            der_act = sum(map(activation_derivative_fn, nn.layers[i].neurons))
+            learn_by = der_act * der_loss
+
+            nn.layers[i - 1].weights = map(weight -> weight - (learn_by * __LEARNING_RATE), nn.layers[i - 1].weights)
+        end
     end
 
     # Randomizes weights between -1, 1
